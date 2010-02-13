71b5ac2f2e8a6b94b5e0d607ef6a2cacd9b51547
Karant <karant@ubuntu.ubuntu-domain>
2010-02-13 12:22:05 -0800

    add aws-s3 gem to environment; replace error pages with standard

--- config/environment.rb
+++ config/environment.rb
@@ -68,6 +68,7 @@ Rails::Initializer.run do |config|
   # config.gem 'mislav-will_paginate', :version => '~> 2.3.2',
   #                                    :lib => 'will_paginate',
   #                                    :source => 'http://gems.github.com'
+  config.gem "aws-s3", :lib => 'aws/s3'
   config.gem 'chronic'
   config.gem 'BlueCloth', :lib => 'bluecloth'
   config.gem 'geokit'
